81035ec04f3af85344d8a0877619d71cba57908d
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-11 20:06:01 +0200

    Clean code

--- static/script.js
+++ static/script.js
@@ -4,22 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const sendMessageBtn = document.getElementById("sendMessage");
   const audioPlayer = document.getElementById("audioPlayer");
 
-  function getCurrentTimeString() {
-    // Create a new Date object to get the current time
-    const currentTime = new Date();
+  let message = "";
 
-    // Get the hours, minutes, and seconds
+  function getCurrentTimeString() {
+    const currentTime = new Date();
     const hours = currentTime.getHours();
     const minutes = currentTime.getMinutes();
     const seconds = currentTime.getSeconds();
-
-    // Format the time as a string
     const timeString = `${hours}:${minutes}:${seconds}`;
 
     return timeString;
   }
-
-  let message = "";
 
   function displayMessage(username, message, className) {
     const isOnline = onlineUsers[username] || false;
@@ -31,7 +26,6 @@ document.addEventListener("DOMContentLoaded", () => {
     chatBox.scrollTop = chatBox.scrollHeight;
   }
 
-  // Generate a random cute username
   function generateRandomUsername() {
     const adjectives = ["Cuddly", "Fluffy", "Psycho", "Sunny", "Bubbly"];
     const animals = ["Kitten", "Puppy", "Bunny", "Duckling", "Panda"];
@@ -44,11 +38,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const USERNAME_ME = generateRandomUsername();
   const onlineUsers = {};
   onlineUsers[USERNAME_ME] = true;
-  // updateOnlineStatus();
 
-  // Connect to the Socket.IO server
   const socket = io.connect();
-  // socket.emit("connect", USERNAME_ME);
   socket.emit("connect_client", USERNAME_ME);
 
   socket.on("refresh_clients", () => {
@@ -64,7 +55,6 @@ document.addEventListener("DOMContentLoaded", () => {
     chatBox.scrollTop = chatBox.scrollHeight;
   }
 
-  // Function to handle audio playback
   function playAudio(songBuffer) {
     const blob = new Blob(songBuffer, { type: "audio/mp3" });
     const audioUrl = URL.createObjectURL(blob);
@@ -73,14 +63,12 @@ document.addEventListener("DOMContentLoaded", () => {
     audioPlayer.autoplay = true;
   }
 
-  // Function to send a message
   function sendMessage() {
     message = messageInput.value;
     if (message.trim() !== "") {
       displayMessage(USERNAME_ME, message, "message");
       messageInput.value = "";
 
-      // Send the message to the server
       fetch("/chat", {
         method: "POST",
         body: `message=${encodeURIComponent(USERNAME_ME + "|" + message)}`,
@@ -100,42 +88,22 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  // Handle Enter key press in the message input field
   messageInput.addEventListener("keydown", (event) => {
     if (event.keyCode === 13) {
-      // If Enter key is pressed, trigger the click event on the "Send" button
       sendMessageBtn.click();
     }
   });
 
   sendMessageBtn.addEventListener("click", sendMessage);
 
-  // socket.on("connect", () => {
-  //   const username = USERNAME_ME; // Change this to get the user's actual username
-  //   onlineUsers[username] = true;
-  //   // Update the user's online status in the chat
-  //   // updateOnlineStatus();
-  //   // Broadcast the updated online user list to all clients
-  //   broadcastOnlineUsers();
-  // });
-
-  // function broadcastOnlineUsers() {
-  //   const onlineUsersList = Object.keys(onlineUsers).filter(
-  //     (username) => onlineUsers[username]
-  //   );
-  //   io.emit("update_online_users", onlineUsersList);
-  // }
-
   socket.on("update_online_users", (onlineUsersList) => {
-    // Update the list of online users in your UI
     const onlineUsersElement = document.getElementById("onlineUsers");
     onlineUsersElement.innerHTML = "Online Users: " + onlineUsersList.message;
   });
 
-  // Listen for song playback command from the server
   socket.on("play_song", (data) => {
     const songPath = `/stream/${data.song}`;
-    const songBuffer = []; // Initialize an array to hold the song chunks
+    const songBuffer = [];
     fetch(songPath)
       .then((response) => {
         const reader = response.body.getReader();
@@ -144,11 +112,11 @@ document.addEventListener("DOMContentLoaded", () => {
             function pump() {
               return reader.read().then(({ done, value }) => {
                 if (done) {
-                  playAudio(songBuffer); // Start playback once the entire song is received
+                  playAudio(songBuffer);
 
                   return;
                 }
-                songBuffer.push(value); // Append the chunk to the buffer
+                songBuffer.push(value);
                 pump();
               });
             }
@@ -161,10 +129,8 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   });
 
-  // Listen for incoming chat messages from the server
   socket.on("chat_message", (data) => {
     if (data.username == USERNAME_ME) {
-      // displayMessage(USERNAME_ME, data.message, "message");
     } else {
       displayMessage(data.username, data.message, "message");
     }
@@ -188,7 +154,6 @@ document.addEventListener("DOMContentLoaded", () => {
     audioPlayer.play();
   });
 
-  // Simulate receiving a welcome message from the server (for demonstration)
   setTimeout(function () {
     displayMessage("Server", "Welcome to the chat!", "server-message");
   }, 1000);
