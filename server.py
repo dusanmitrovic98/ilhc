0943c074a8b27e112b8ef450df1a83ddcc5695c1
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-11 21:20:41 +0200

    Bigger update

--- server.py
+++ server.py
@@ -80,3 +80,84 @@ def chat():
                 new_name = message.split(' ')[2]
                 songs = os.listdir(music_folder)
                 if 1 <= song_number <= len(songs):
+                    old_name = songs[song_number - 1]
+                    old_path = os.path.join(music_folder, old_name)
+                    new_path = os.path.join(music_folder, new_name)
+                    try:
+                        os.rename(old_path, new_path)
+                        server_response(f'Song "{old_name}" renamed to "{new_name}"')
+                    except OSError as e:
+                        server_response(f'Error renaming song: {str(e)}')
+                    return ""
+                else:
+                    return "Invalid song number."
+            except ValueError:
+                return "Invalid command format."
+        elif message == '/list':
+            songs = os.listdir(music_folder)
+            i = 1
+            for song in songs:
+                server_response(f"{i}. {song} ❤️ ")
+                i += 1
+            return ''
+        elif message.startswith('/play ') or message.startswith('/p ') or message.startswith('/ '):
+            try:
+                song_number = int(message.split(' ')[1])
+                songs = os.listdir(music_folder)
+                if 1 <= song_number <= len(songs):
+                    song_to_play = songs[song_number - 1]
+                    socketio.emit("play_song", {"song": song_to_play})
+                    socketio.emit("chat_message", {"username": "Server", "message": "Buffering \"" + song_to_play + "\"..."})
+                    return ""
+                else:
+                    return "Invalid song number."
+            except ValueError:
+                return "Invalid command format."
+        elif message.startswith('/download '):
+            url = message.split(' ')[1]
+            print(url)
+            download_from_url(url)
+        else:
+            chat_messages.append(message)
+            socketio.emit('chat_message', {'username': username, 'message': message})
+            return ""
+    return ""
+
+def clear_chat(username):
+    socketio.emit("clear_chat")
+    socketio.emit("chat_message", {"username": "Server", "message": username + " cleared the chat..."})
+
+def download_from_url(url):
+    socketio.emit("chat_message", {"username": "Server", "message": url})
+    youtube = YouTube(url, on_progress_callback=on_progress)
+    out_file = youtube.streams.filter(only_audio=True).first().download(music_folder)
+    os.rename(out_file, os.path.splitext(out_file)[0] + ".mp3")
+    server_response('"' + os.path.basename(os.path.splitext(out_file)[0] + ".mp3") + '" downloaded successfully.')
+    
+def server_response(response):
+    socketio.emit("chat_message", {"username": NAME_SERVER, "message": response})
+
+def play_song_from_start():
+    socketio.emit("play_song_from_start")
+
+def song_pause():
+    socketio.emit("song_pause")
+
+def song_resume():
+    socketio.emit("song_resume")
+
+def help(): 
+    server_response('/online')
+    server_response('/list')
+    server_response('/play song_number')
+    server_response('/p song_number')
+    server_response('/ song_number')
+    server_response('/download song_url')
+    server_response('/rename song_number new_name.mp3')
+    server_response('/start')
+    server_response('/pause')
+    server_response('/resume')
+    server_response('/clear')
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
