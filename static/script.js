81fdafe140fbcf8d71a00b796ad1f2401515ffa3
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-16 19:59:49 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -31,3 +31,4 @@ document.addEventListener("DOMContentLoaded", () => {
     const randomAdjective =
       adjectives[Math.floor(Math.random() * adjectives.length)];
     const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
+    return `${randomAdjective} ${randomAnimal}`;
