439f38f9879e8c13df2bf7116d36733b06861100
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-17 21:16:55 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,17 +1,1 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const chatBox = document.getElementById("chatBox");
-  const messageInput = document.getElementById("messageInput");
-  const sendMessageBtn = document.getElementById("sendMessage");
-  const audioPlayer = document.getElementById("audioPlayer");
-  const timer = document.getElementById("timer");
-
-  let message = "";
-
-  function getCurrentTimeString() {
-    const currentTime = new Date();
-    const hours = currentTime.getHours();
-    const minutes = currentTime.getMinutes();
-    const seconds = currentTime.getSeconds();
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    return timeString;
-  }
