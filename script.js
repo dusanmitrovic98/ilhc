e9d18b1d14b95a42bf5ac8e63862b1b1ee53a073
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-28 19:09:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,4 @@ document.addEventListener("DOMContentLoaded", () => {
     const animals = ["Kitten", "Puppy", "Bunny", "Duckling", "Panda"];
     const randomAdjective =
       adjectives[Math.floor(Math.random() * adjectives.length)];
+    const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
