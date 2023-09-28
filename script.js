e57a36b24636069b1fe94837c94c1c7d880dd0af
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-28 19:01:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
   const chatBox = document.getElementById("chatBox");
   const messageInput = document.getElementById("messageInput");
+  const sendMessageBtn = document.getElementById("sendMessage");
