ba79c1c2145a2247825cf33065696ac356b90ace
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-28 19:30:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -96,3 +96,4 @@ document.addEventListener("DOMContentLoaded", () => {
   sendMessageBtn.addEventListener("click", sendMessage);
 
   socket.on("update_online_users", (onlineUsersList) => {
+    const onlineUsersElement = document.getElementById("onlineUsers");
