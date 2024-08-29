c188af26850424feb818412ac1176cbb93232816
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2024-08-29 19:10:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,1 @@
 import threading
-import time
-from flask import Flask, Response, render_template, request
-from flask_socketio import SocketIO
-from pytube.cli import on_progress
-from pymongo import MongoClient
-from datetime import datetime
-from moviepy.editor import AudioFileClip
-from pytube import YouTube
-import sys
-import os
-import random
-
-available_songs = []  # Add your available songs to this list
-is_random_playing = False
-allrandom = True
